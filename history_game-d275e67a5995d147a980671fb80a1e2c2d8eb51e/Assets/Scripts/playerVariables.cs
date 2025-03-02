d2be6b1eab9d196dd77ce62f0e7ba7825e0ed7ee
Chi Anh <lechianh04@gmail.com>
2025-03-02 20:57:08 +0700

    chianh2

---
+++ history_game-d275e67a5995d147a980671fb80a1e2c2d8eb51e/Assets/Scripts/playerVariables.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerVariables : MonoBehaviour
+{
+    // Static variables
+    public static bool isWalking, isJumping, isGrabbing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initial log, only logs once when the game starts
+        Debug.Log("Initial values: isWalking = " + isWalking + ", isJumping = " + isJumping + ", isGrabbing = " + isGrabbing);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Log the values at the same position every frame
+        UpdateLog();
+
+    }
+
+    // Method to update the log without stacking new logs
+    private void UpdateLog()
+    {
+        // This clears the console (optional)
+
+
+        // This will log the current values of isWalking, isJumping, and isGrabbing at the same position
+        Debug.Log("isWalking: " + isWalking + ", isJumping: " + isJumping + ", isGrabbing: " + isGrabbing);
+    }
+
+}
