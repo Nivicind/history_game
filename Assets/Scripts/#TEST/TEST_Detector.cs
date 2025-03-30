d19a68ed39880f7b88118ef4accc7a439d26c2e2
Đoàn Nguyễn Nam <163382932+Nivicind@users.noreply.github.com>
2025-03-30 21:37:29 +0700

    0.0.15
    chi anh them may tinh nag di cho hay

--- Assets/Scripts/#TEST/TEST_Detector.cs
+++ Assets/Scripts/#TEST/TEST_Detector.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TEST_Detector : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 3f;
+    public float moveDistance = 5f;
+    private Vector3 startPos;
+    private int direction = 1;
+
+    private bool playerIsHidden = false;
+
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Move left and right
+        transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
+
+        // Reverse direction when reaching limits
+        if (Mathf.Abs(transform.position.x - startPos.x) > moveDistance)
+        {
+            direction *= -1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !playerIsHidden)
+        {
+            Debug.Log("Player Detected!");
+        }
+    }
+
+    public void SetPlayerHidden(bool isHidden)
+    {
+        playerIsHidden = isHidden;
     }
 }
