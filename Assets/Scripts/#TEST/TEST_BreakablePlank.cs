ab210dbd47ed684648d8fa058a750cb7b3be8ba8
Đoàn Nguyễn Nam <163382932+Nivicind@users.noreply.github.com>
2025-05-13 22:40:33 +0700

    asdasd

--- Assets/Scripts/#TEST/TEST_BreakablePlank.cs
+++ Assets/Scripts/#TEST/TEST_BreakablePlank.cs
@@ -1,33 +1,57 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TEST_BreakablePlank : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] private string boxTag = "Box"; // Tag for detecting the box
-    [SerializeField] private float fallThreshold = -2f; // Minimum falling speed to trigger
+    [SerializeField] private string boxTag = "Box"; // Tag to detect the box
+    [SerializeField] private float fallThreshold = -2f; // Velocity to trigger breaking
 
     private Collider2D objCollider;
+    private Animator animator;
+    private bool isBreaking = false;
 
     void Start()
     {
         objCollider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Check if the object that collided is a "Box"
+        if (isBreaking) return;
+
         if (collision.gameObject.CompareTag(boxTag))
         {
-            Rigidbody2D boxRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
 
-            // Check if the box is actually falling
-            if (boxRb != null && boxRb.velocity.y < fallThreshold)
+            if (rb != null && rb.velocity.y < fallThreshold)
             {
-                // objCollider.enabled = false; // Disable collider
-                gameObject.SetActive(false); // Deactivate the plank
+                isBreaking = true;
+                objCollider.enabled = false; // Disable plank collision
+                animator.SetTrigger("Break"); // Trigger the animation
+                StartCoroutine(DisableAfterAnimation());
             }
         }
+    }
+
+    private IEnumerator DisableAfterAnimation()
+    {
+        // Wait for the animation clip to finish playing
+        AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
+        float breakAnimLength = 0f;
+
+        // Find the length of the Plank Breaking animation
+        foreach (var clip in clips)
+        {
+            if (clip.name == "Plank Breaking") // Make sure this matches your clip name
+            {
+                breakAnimLength = clip.length;
+                break;
+            }
+        }
+
+        yield return new WaitForSeconds(breakAnimLength);
+        gameObject.SetActive(false); // Disable the plank object after the animation
     }
 }
